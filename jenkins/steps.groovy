2f5a7fadfbd39d3747242c0111d596f7a4d36fc0
Martin Proffitt <martin.proffitt@hpe.com>
2019-11-01 12:05:16 +0100

    Update Jenkinsfile

---
+++ jenkins/steps.groovy
@@ -0,0 +1,14 @@
+
+def structure() {
+    sh """
+    |if [ -d code ] ; then
+    |    find . -maxdepth 1 -type f -exec rm {} \\;
+    |    mv code/* .
+    |    rm -rf code
+    |    echo "==================================================="
+    |    ls
+    |    echo "==================================================="
+    |fi
+    |""".stripMargin()
+}
+
